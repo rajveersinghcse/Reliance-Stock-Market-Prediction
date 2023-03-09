e97f9ae7f734eb4a3e3b31949989d4ba895806b0
Rajveer Singh <117706744+rajveersinghcse@users.noreply.github.com>
2023-03-09 12:25:52 +0530

    new

--- app.py
+++ app.py
@@ -1,238 +1,34 @@
-import time
-import math
-import numpy as np
 import pandas as pd
-import seaborn as sns
-import datetime as dt
-import yfinance as yf
-import streamlit as st
-import plotly.express as px
 import matplotlib.pyplot as plt
-from tqdm.notebook import tqdm
-from tensorflow import keras
-from datetime import date
-from plotly import graph_objs as go
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
+import seaborn as sns
+import yfinance as yf
+import datetime
+import streamlit as st
+import model_building as m
 
 
+with st.sidebar:
+    st.markdown("# Reliance Stock Market Prediction")
+    user_input = st.multiselect('Please select the stock',['RELIANCE.NS'],['RELIANCE.NS'])
 
-START = "2015-01-01"
-TODAY = dt.datetime.now().strftime("%Y-%m-%d")
+    # user_input = st.text_input('Enter Stock Name', "ADANIENT.NS")
+    st.markdown("### Choose Date for your anaylsis")
+    START = st.date_input("From",datetime.date(2015, 1, 1))
+    END = st.date_input("To",datetime.date(2023, 2, 25))
+    bt = st.button('Submit') 
 
-stocks = ["RELIANCE.NS"]
+#adding a button
+if bt:
 
-
-# Loading Data ---------------------
-
-#@st.cache(suppress_st_warning=True)
-def load_data(ticker):
-    data = yf.download(ticker, START,  TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-
-#For Stock Financials ----------------------
-
-def stock_financials(stock):
-    df_ticker = yf.Ticker(stock)
-    sector = df_ticker.info['sector']
-    prevClose = df_ticker.info['previousClose']
-    marketCap = df_ticker.info['marketCap']
-    twoHunDayAvg = df_ticker.info['twoHundredDayAverage']
-    fiftyTwoWeekHigh = df_ticker.info['fiftyTwoWeekHigh']
-    fiftyTwoWeekLow = df_ticker.info['fiftyTwoWeekLow']
-    Name = df_ticker.info['longName']
-    averageVolume = df_ticker.info['averageVolume']
-    ftWeekChange = df_ticker.info['52WeekChange']
-    website = df_ticker.info['website']
-
-    st.write('Company Name -', Name)
-    st.write('Sector -', sector)
-    st.write('Company Website -', website)
-    st.write('Average Volume -', averageVolume)
-    st.write('Market Cap -', marketCap)
-    st.write('Previous Close -', prevClose)
-    st.write('52 Week Change -', ftWeekChange)
-    st.write('52 Week High -', fiftyTwoWeekHigh)
-    st.write('52 Week Low -', fiftyTwoWeekLow)
-    st.write('200 Day Average -', twoHunDayAvg)
-
-
-#Plotting Raw Data ---------------------------------------
-
-def plot_raw_data(stock, data_1):
-    df_ticker = yf.Ticker(stock)
-    Name = df_ticker.info['longName']
-    #data1 = df_ticker.history()
-    data_1.reset_index()
-    #st.write(data_1)
-    numeric_df = data_1.select_dtypes(['float', 'int'])
-    numeric_cols = numeric_df.columns.tolist()
-    st.markdown('')
-    st.markdown('**_Features_** you want to **_Plot_**')
-    features_selected = st.multiselect("", numeric_cols)
-    if st.button("Generate Plot"):
-        cust_data = data_1[features_selected]
-        plotly_figure = px.line(data_frame=cust_data, x=data_1['Date'], y=features_selected,
-                                title= Name + ' ' + '<i>timeline</i>')
-        plotly_figure.update_layout(title = {'y':0.9,'x':0.5, 'xanchor': 'center', 'yanchor': 'top'})
-        plotly_figure.update_xaxes(title_text='Date')
-        plotly_figure.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01, title="Price"), width=800, height=550)
-        st.plotly_chart(plotly_figure)
-
-
-#For LSTM MOdel ------------------------------
-
-def create_train_test_LSTM(df, epoch, b_s, ticker_name):
-
-    df_filtered = df.filter(['Close'])
-    dataset = df_filtered.values
-
-    #Training Data
-    training_data_len = math.ceil(len(dataset) * .7)
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(dataset)
-
-    train_data = scaled_data[0: training_data_len, :]
-
-    x_train_data, y_train_data = [], []
-
-    for i in range(60, len(train_data)):
-        x_train_data.append(train_data[i-60:i, 0])
-        y_train_data.append(train_data[i, 0])
-
-    x_train_data, y_train_data = np.array(x_train_data), np.array(y_train_data)
-
-    x_train_data = np.reshape(x_train_data, (x_train_data.shape[0], x_train_data.shape[1], 1))
-
-    #Testing Data
-    test_data = scaled_data[training_data_len - 60:, :]
-
-    x_test_data = []
-    y_test_data = dataset[training_data_len:, :]
-    
-    for j in range(60, len(test_data)):
-        x_test_data.append(test_data[j - 60:j, 0])
-
-    x_test_data = np.array(x_test_data)
-
-    x_test_data = np.reshape(x_test_data, (x_test_data.shape[0], x_test_data.shape[1], 1))
-
-
-    model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train_data.shape[1], 1)))
-    model.add(LSTM(units=50, return_sequences=False))
-
-    model.add(Dense(25))
-    model.add(Dense(1))
-
-    model.compile(optimizer='adam', loss='mean_squared_error')
-
-    model.fit(x_train_data, y_train_data, batch_size=int(b_s), epochs=int(epoch))
-    st.success("Your Model is Trained Succesfully!")
-    st.markdown('')
-    st.write("Predicted vs Actual Results for LSTM")
-    st.write("Stock Prediction for 30 days - ",ticker_name)
-
-    predictions = model.predict(x_test_data)
-    predictions = scaler.inverse_transform(predictions)
-
-    train = df_filtered[:training_data_len]
-    valid = df_filtered[training_data_len:]
-    valid['Predictions'] = predictions
-
-    new_valid = valid[::-1]
-    rev_valid = new_valid[:30]
-    rev_valid_30 = rev_valid[::-1]
-    n_valid = rev_valid_30.reset_index()
-    n_valid.drop('index', inplace=True, axis=1)
-    st.dataframe(n_valid)
-    st.markdown('')
-    st.write("Plotting Actual vs Predicted ")
-
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    plt.figure(figsize=(14, 8))
-    plt.title('Actual Close prices vs Predicted Using LSTM Model', fontsize=20)
-    plt.plot(valid[['Close', 'Predictions']])
-    plt.legend(['Actual', 'Predictions'], loc='upper left', prop={"size":20})
-    st.pyplot()
-
-
-
-#Creating Training and Testing Data for other Models ----------------------
-
-def create_train_test_data(df1):
-
-    data = df1.sort_index(ascending=True, axis=0)
-    new_data = pd.DataFrame(index=range(0, len(df1)), columns=['Date', 'High', 'Low', 'Open', 'Volume', 'Close'])
-
-    for i in range(0, len(data)):
-        new_data['Date'][i] = data['Date'][i]
-        new_data['High'][i] = data['High'][i]
-        new_data['Low'][i] = data['Low'][i]
-        new_data['Open'][i] = data['Open'][i]
-        new_data['Volume'][i] = data['Volume'][i]
-        new_data['Close'][i] = data['Close'][i]
-
-    #Removing the hour, minute and second
-    new_data['Date'] = pd.to_datetime(new_data['Date']).dt.date
-
-    train_data_len = math.ceil(len(new_data) * .8)
-
-    train_data = new_data[:train_data_len]
-    test_data = new_data[train_data_len:]
-    return train_data, test_data
-
-#Plotting the Predictions -------------------------
-
-
-def prediction_plot(pred_data, test_data, models, ticker_name):
-
-    test_data['Predicted'] = 0
-    test_data['Predicted'] = pred_data
-
-    #Resetting the index
-    test_data.reset_index(inplace=True, drop=True)
-    st.success("Your Model is Trained Succesfully!")
-    st.markdown('')
-    st.write("Predicted Price vs Actual Close Price Results for - " ,models)
-    st.write("Stock Prediction for next 30 days - ", ticker_name)
-    st.write(test_data[['Date', 'Close', 'Predicted']])
-    st.write("Plotting Close Price vs Predicted Price for - ", models)
-
-    #Plotting the Graph
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=test_data['Date'], y=test_data['Close'], mode='lines', name='Close'))
-    fig.add_trace(go.Scatter(x=test_data['Date'], y=test_data['Predicted'], mode='lines', name='Predicted'))
-    fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01), height=550, width=800,
-                      autosize=False, margin=dict(l=25, r=75, b=100, t=0))
-
-    st.plotly_chart(fig)
-
-
-
-# Sidebar Menu -----------------------
-
-menu=["EDA","Train Model"]
-st.sidebar.title("Settings")
-st.sidebar.subheader("Timeseries Settings")
-choices = st.sidebar.selectbox("Select the Activity", menu,index=0)
-
-
-
-if choices == 'EDA':
-    
-    # Importing dataset
-    reliance_0 = yf.download('RELIANCE.NS', start='2015-01-01')
-    reliance_0.reset_index(inplace = True)
+# Importing dataset------------------------------------------------------
+    df = yf.download('RELIANCE.NS', start=START, end=END)
+    plotdf, future_predicted_values =m.create_model(df)
+    df.reset_index(inplace = True)
     st.title('Reliance Stock Market Prediction')
     st.header("Data We collected from the source")
-    st.write(reliance_0)
+    st.write(df)
 
-    reliance_1=reliance_0.drop(["Adj Close"],axis=1).reset_index(drop=True)
+    reliance_1=df.drop(["Adj Close"],axis=1).reset_index(drop=True)
     reliance_2=reliance_1.dropna().reset_index(drop=True)
 
     reliance=reliance_2.copy()
@@ -275,7 +71,7 @@ if choices == 'EDA':
     plt.title('Low')
     st.pyplot()
 
-# ------------------------box-plots---------------------------------
+#------------------------box-plots---------------------------------
 
     # Creating box-plots
     st.header("Box Plots")
@@ -307,7 +103,7 @@ if choices == 'EDA':
     plt.title('Low')
     st.pyplot()
 
-# ----------------------Histogram---------------------------------------
+#---------------------Histogram---------------------------------------
 
     st.header("Histogram")
     # Ploting Histogram
@@ -339,7 +135,7 @@ if choices == 'EDA':
     st.pyplot()
 
 
-# -------------------------KDE Plots-----------------------------------------
+#-------------------------KDE Plots-----------------------------------------
 
     st.header("KDE Plots")
     # KDE-Plots
@@ -367,7 +163,7 @@ if choices == 'EDA':
     st.line_chart(reliance['Volume'])
 
 
-# -------------------Finding long-term and short-term trends---------------------
+#-------------------Finding long-term and short-term trends---------------------
 
     st.title('Finding long-term and short-term trends')
     reliance_ma=reliance.copy()
@@ -395,32 +191,18 @@ if choices == 'EDA':
     plt.xlabel('Date')
     plt.ylabel('Price')
     st.pyplot()
-# -----------------------------Train Model----------------------------------
 
-elif choices == 'Train Model':
-    st.subheader("Train Machine Learning Models for Stock Prediction")
-    st.markdown('')
-    st.markdown('**_Select_ _Stocks_ _to_ Train**')
-    stock_select = st.selectbox("", stocks, index=0)
-    df1 = load_data(stock_select)
-    df1 = df1.reset_index()
-    df1['Date'] = pd.to_datetime(df1['Date']).dt.date
-    options = ['LSTM']
-    st.markdown('')
-    st.markdown('**_Select_ _Machine_ _Learning_ _Algorithms_ to Train**')
-    models = st.selectbox("", options)
-    submit = st.button('Train Model')
+    df1 = pd.DataFrame(future_predicted_values)
+    st.markdown("### Next 30 days forecast")
+    df1.rename(columns={0: "Predicted Prices"}, inplace=True)
+    st.write(df1)
 
-    if models == 'LSTM':
-        st.markdown('')
-        st.markdown('')
-        st.markdown("**Select the _Number_ _of_ _epochs_ and _batch_ _size_ for _training_ form the following**")
-        st.markdown('')
-        epoch = st.slider("Epochs", 0, 300, step=1)
-        b_s = st.slider("Batch Size", 32, 1024, step=1)
-        if submit:
-            st.write('**Your _final_ _dataframe_ _for_ Training**')
-            st.write(df1[['Date','Close']])
-            create_train_test_LSTM(df1, epoch, b_s, stock_select)
-
-# -------------------------------------------------------------------------------------------
+    st.markdown("### Original vs predicted close price")
+    fig= plt.figure(figsize=(20,10))
+    sns.lineplot(data=plotdf)
+    st.pyplot(fig)
+    
+    
+else:
+    #displayed when the button is unclicked
+    st.write('Please click on the submit button to get the EDA ans Prediction') 
---
+++ model_building.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM, GRU
+from sklearn.preprocessing import MinMaxScaler
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+def create_model(df):
+    pass
+    scaler=MinMaxScaler(feature_range=(0,1))
+    closedf=scaler.fit_transform(np.array(df['Close']).reshape(-1,1))
+
+    training_size=int(len(closedf)*0.75)
+    test_size=len(closedf)-training_size
+    train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
+
+    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
+    time_step = 15
+    X_train, y_train = create_dataset(train_data, time_step)
+    X_test, y_test = create_dataset(test_data, time_step)
+
+    # reshape input to be [samples, time steps, features] which is required for LSTM
+    # LSTM requies 3-dimensional input
+    X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+    X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
+
+    # Model building
+    tf.keras.backend.clear_session()
+    model=Sequential()
+    model.add(LSTM(32,return_sequences=True,input_shape=(time_step,1)))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1))
+    optimizer = tf.keras.optimizers.Adam()
+    model.compile(loss='mean_squared_error',optimizer=optimizer)
+    model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=100,batch_size=32,verbose=1)
+
+    ### Lets Do the prediction and check performance metrics
+    train_predict=model.predict(X_train)
+    test_predict=model.predict(X_test)
+
+    # Transform back to original form
+    train_predict = scaler.inverse_transform(train_predict)
+    test_predict = scaler.inverse_transform(test_predict)
+    original_ytrain = scaler.inverse_transform(y_train.reshape(-1,1)) 
+    original_ytest = scaler.inverse_transform(y_test.reshape(-1,1)) 
+
+    # shift train predictions for plotting
+    look_back=time_step
+    trainPredictPlot = np.empty_like(closedf)
+    trainPredictPlot[:, :] = np.nan
+    trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+
+    # shift test predictions for plotting
+    testPredictPlot = np.empty_like(closedf)
+    testPredictPlot[:, :] = np.nan
+    testPredictPlot[len(train_predict)+(look_back*2)+1:len(closedf)-1, :] = test_predict
+
+    plotdf = pd.DataFrame({'Date': df.index,'original_close': df['Close'],'train_predicted_close': trainPredictPlot.reshape(1,-1)[0].tolist(),
+                        'test_predicted_close': testPredictPlot.reshape(1,-1)[0].tolist()})
+
+    x_input=test_data[len(test_data)-time_step:].reshape(1,-1)
+    temp_input=list(x_input)
+    temp_input=temp_input[0].tolist()
+    from numpy import array
+    lst_output=[]
+    n_steps=time_step
+    i=0
+    pred_days = 30
+    while(i<pred_days):
+        if(len(temp_input)>time_step):
+            x_input=np.array(temp_input[1:])
+            x_input=x_input.reshape(1,-1)
+            yhat = model.predict(np.expand_dims(x_input, 2))
+            temp_input.extend(yhat[0])
+            temp_input=temp_input[1:]
+        
+            lst_output.extend(yhat.tolist())
+            i=i+1
+            
+        else:
+            yhat = model.predict(np.expand_dims(x_input, 2))
+            temp_input.extend(yhat[0])
+            lst_output.extend(yhat.tolist())
+            
+            i=i+1
+    next_predicted_days_value = scaler.inverse_transform(np.array(lst_output).reshape(-1,1)).reshape(1,-1).tolist()[0]
+    return plotdf,pd.DataFrame(next_predicted_days_value)
